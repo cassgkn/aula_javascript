deae78fe0d7eae52fe7756e1e436c75dc7f22ba3
cassgkn <cassgkn@hotmail.com>
2022-07-11 17:53:13 -0300

    codigos var e conversoes

---
+++ conversoes.js
@@ -0,0 +1,9 @@
+// conversão implicita
+const numero = 456;
+const numeroString = Number('456');
+
+//Number()
+//String()
+console.log(numero + numeroString);
+
+// conversão explicita
---
+++ truthy e falsy.js
@@ -0,0 +1,8 @@
+// truthy ou falsy
+// 0 = false
+// 1 = ture
+
+console.log(0 == false)
+console.log("" == false)
+console.log(1 == true)
+
